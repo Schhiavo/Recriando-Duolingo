5329e18f390a17896f5cc553f8037961bcf54e2d
Victor Schiavo <victorschiavo343@gmail.com>
2022-10-26 09:38:32 -0300

    criando um evento de click(ouvinte) que vai identificar se a alternativa clicada é a certa

--- script.js
+++ script.js
@@ -11,8 +11,19 @@ function mostraquestao(q){
      let alts = document.querySelectorAll('.alternativa'); // alts vai receber todos os elementos html que possuem a classe "alternativa"
 
      alts.forEach(function(element, index){
-        element.textContent = q.alternativas[index];
-     }) // um loop usando ForEach(para cada), que para cada elemento guardado em alts, ele vai exibir um conteudo do array alternativas, começando pelo indice 0
+        element.textContent = q.alternativas[index];// um loop usando ForEach(para cada), que para cada elemento guardado em alts, ele vai exibir um conteudo do array alternativas, começando pelo indice 0
+
+        element.addEventListener('click', function(){
+         if(index == q.correta){
+            console.log("Acertou!")
+         }
+         else{
+            console.log("Errou!")
+         }
+        }) // um ouvinte que vai identificar quando o mouse clicar nas alternativas e vai chamar a função que vai verificar se a alternativa clicada possui o indice correto do array
+     })
+
 }
 
 mostraquestao(pergunta);
+
