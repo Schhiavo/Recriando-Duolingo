4cd91b9d68251c54ff561eee1a0c2318619d9b25
Victor Schiavo <victorschiavo343@gmail.com>
2022-10-26 10:26:29 -0300

    Transformando as funções em metodos do objeto app, transformado o objeto pergunta num array de "perguntas". Adicionado o metodo checa resposta

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-let pergunta = {
+let perguntas = [
+{
    titulo: 'Gato',
    alternativas: ['Gat', 'Cat', 'Gate', 'Dog'],
    correta: 1
-}; //criado objeto pergunta
+},
+{
+   titulo: 'Cachorro',
+   alternativas: ['Cat', 'Doge', 'Dodge', 'Dog'],
+   correta: 3
+},
+{
+   titulo: 'Água',
+   alternativas: ['What', 'Whater', 'Water', 'Watter'],
+   correta: 2
+}
+]
 
-function start(){
+let app = {
+
+ start: function(){
 
    let alts = document.querySelectorAll('.alternativa');
-   alts.forEach(function(element, index){
-      element.addEventListener('click', function(){
-         if(index == pergunta.correta){
-            console.log("Acertou!");
-         }
-         else{
-            console.log("Errou!");
-         }
+   alts.forEach((element, index)=>{
+      element.addEventListener('click', ()=>{
+        this.checaResposta(index);
       })
    })
-    mostraquestao(pergunta);
-} //uma nova função para sempre adicionar um evento a cada questão criada no app, que vai ser responsavel por "ouvir" o evento click do mouse e verificar se a alternativa está correta
+    this.mostraquestao(perguntas);
+}, //uma nova função para sempre adicionar um evento a cada questão criada no app, que vai ser responsavel por "ouvir" o evento click do mouse e verificar se a alternativa está correta
 
-function mostraquestao(q){
+mostraquestao: function(q){
+   this.qatual = q;
      let titleDiv = document.getElementById('titulo'); // titleDiv está recebendo o elemento do id título
      titleDiv.textContent = q.titulo; //O conteudo de texto do elemento DOM pego pelo titleDiv, vai receber o título do objeto pergunta que foi passado como parametro na função
 
@@ -29,8 +39,15 @@ function mostraquestao(q){
      alts.forEach(function(element, index){
         element.textContent = q.alternativas[index];// um loop usando ForEach(para cada), que para cada elemento guardado em alts, ele vai exibir um conteudo do array alternativas, começando pelo indice 0
      })
-
+},
+checaResposta: function(user){
+     if(this.qatual.correta == user){
+      console.log("Correta");
+     }
+     else{
+      console.log("Errada");
+     }
 }
-
-start();
+}
+app.start();
 
